ee8b7e3ea58b1761ba028848b785a6fbd74d80bc
subeenyoo <subeenyoo1002@gmail.com>
2025-08-25 12:41:20 +0900

    누락된 pch 헤더 추가

---
+++ Engine/Core/pch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Windows.h>
+#include <d3d12.h>
+#include <D3Dcompiler.h>
+#include <DirectXHelpers.h>
+#include <dxgi1_4.h>
+#include <comdef.h>
+#include <DirectXColors.h>
+
+#include "engine.h"
+
+#if defined(DEBUG) || defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+
+// Link necessary d3d12 libraries.
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+
+// namespace
+using namespace DirectX;
